b2772a06ad7c974e7f43039d88aae4e7ede429d4
Roxon12 <97814828+Roxon12@users.noreply.github.com>
2025-02-15 21:27:48 -0500

    get-events.js

--- get-events.js
+++ get-events.js
@@ -1,21 +1,24 @@
 const fetch = require('node-fetch'); // O cualquier otra librería para hacer solicitudes HTTP
 
-// Esta función se ejecutará cada vez que se realice una solicitud a la URL correspondiente en Netlify
 exports.handler = async function(event, context) {
-    // Realiza una solicitud GET a la API de fútbol
     const response = await fetch('https://api.football-api.com/v2/matches', {
         method: 'GET',
         headers: {
-            'Authorization': 'Bearer TU_API_KEY', // Reemplaza 'TU_API_KEY' con tu clave real
+            'Authorization': `Bearer ${process.env.FOOTBALL_API_KEY}`, // Asegúrate de que la clave esté bien configurada
         }
     });
-    
-    // Espera la respuesta de la API
+
+    if (!response.ok) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error fetching events' }),
+        };
+    }
+
     const data = await response.json();
     
-    // Devuelve la respuesta con el código de estado 200 (OK) y los datos en formato JSON
     return {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
     };
 };
